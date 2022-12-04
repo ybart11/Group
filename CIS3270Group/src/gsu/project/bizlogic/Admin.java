ac922de22c295ff8b0538cc22221966730305de9
yovany_23 <yovany_23@localhost>
2022-12-04 16:31:05 -0500

    Created admin class, added data field, constructors, and method

---
+++ CIS3270Group/src/gsu/project/bizlogic/Admin.java
@@ -0,0 +1,25 @@
+package gsu.project.bizlogic;
+
+public class Admin extends Customer {
+
+	public final static Boolean isAdmin = true;
+	
+	public Admin (String userName, String password) {
+		super(userName, password);
+	}
+
+	public Admin(int customerID, String firstName, String lastName, String address, int zip, String state,
+			String userName, String password, String email, int ssn, String securityQuestion, String securityAnswer) {
+		super(customerID, firstName, lastName, address, zip, state, userName, password, email, ssn, securityQuestion,
+				securityAnswer);
+
+	}
+	
+	@Override
+	public String toString() {
+		return "\nUsername: " + getUserName() + "\npassword: " + getPassword()
+		+ "\nAdmin status: " + isAdmin;
+	}
+	
+	
+}
