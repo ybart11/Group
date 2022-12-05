746259378c48fe3f99ae5d0b818a3a391a222cbb
p2sca <p2sca@10.250.89.23>
2022-12-05 17:15:47 -0500

    Updated GUI

--- CIS3270Group/src/gsu/project/gui/RecoverPassword.java
+++ CIS3270Group/src/gsu/project/gui/RecoverPassword.java
@@ -1,6 +1,14 @@
 package gsu.project.gui;
 
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class RecoverPassword {
 
