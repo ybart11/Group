6fcca06cbc920f1e98ac44522b6bb90243b7918b
yovany_23 <yovany_23@localhost>
2022-12-06 02:17:55 -0500

    added findFlights class with try and catch

--- CIS3270Group/src/gsu/project/database/SearchDB.java
+++ CIS3270Group/src/gsu/project/database/SearchDB.java
@@ -1,5 +1,70 @@
 package gsu.project.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import gsu.project.bizlogic.Flight;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class SearchDB {
+	
+	static final String driver = "com.mysql.cj.jdbc.Driver";
+	static final String databaseURL = "jdbc:mysql://localhost:3306/AirlineReservation";
+	static final String databaseUsername = "root";
+	static final String databasePassword = "";
+	
+	
+	public static ObservableList<Flight> findFlights(String departFrom,String arriveTo,
+			String departDay,String returnDay){
+		
+		ObservableList<Flight> flights = FXCollections.observableArrayList();
+
+		
+		try {
+			
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+
+			PreparedStatement ps = 
+					con.prepareStatement("SELECT * FROM flights WHERE departure_city=" + "'" + departFrom + "'" + 
+			"and destination_city=" + "'" + arriveTo + "'" + "and flight_date=" + "'" + departDay + "'" 
+							+ "and return_day=" + "'" + returnDay + "'");
+
+			ResultSet rs = ps.executeQuery();
+			
+			
+			
+			while(rs.next()) {
+				
+				Flight flight = new Flight();
+				
+				flight.setFlightNum(rs.getInt("flight_number"));
+				flight.setDepartureCity(rs.getString("departure_city"));
+				flight.setDestinationCity(rs.getString("destination_city"));
+				flight.setDepartTime(rs.getString("depart_time"));
+				flight.setArriveTime(rs.getString("arrive_time"));
+				flight.setFlightDate(rs.getString("flight_date"));
+				flight.setReturnFlight(rs.getString("return_day"));
+				flight.setNumOfSeats(rs.getInt("num_seats"));
+				
+				flights.add(flight);
+				
+				
+			}
+			
+			con.close();
+				
+		
+	} catch(Exception ex) {
+		
+		System.out.println("Something went wrong with the database!");
+	
+		
+	}
+		return flights;
+		
+}
 
 }
