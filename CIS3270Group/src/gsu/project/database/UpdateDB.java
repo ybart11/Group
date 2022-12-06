b818e688c27c33a68d94cfb7e42a599aeb2e2059
yovany_23 <yovany_23@localhost>
2022-12-06 02:11:59 -0500

    Added deleteFlight and deleteBooking classes

--- CIS3270Group/src/gsu/project/database/UpdateDB.java
+++ CIS3270Group/src/gsu/project/database/UpdateDB.java
@@ -1,5 +1,77 @@
 package gsu.project.database;
 
-public class UpdateDB {
+import java.sql.*;
+import gsu.project.bizlogic.Flight;
 
+public class UpdateDB {
+	
+	static final String driver = "com.mysql.cj.jdbc.Driver";
+	static final String databaseURL = "jdbc:mysql://localhost:3306/AirlineReservation";
+	static final String databaseUsername = "root";
+	static final String databasePassword = "";
+	
+	public static Boolean result; 
+	
+	public static void deleteFlight(int flightNum) {
+		
+		result = false; 
+		
+		try {
+			
+			Class.forName(driver);
+
+			Connection con = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+			
+			PreparedStatement ps = 
+					con.prepareStatement("DELETE FROM flights WHERE flight_number=" + "'" + flightNum + "'");
+
+			// ps.setInt(1, flightNum);
+			
+			ps.executeUpdate();
+		
+			con.close();
+			
+			result = true;
+			
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+			result = false;
+		
+		}
+		
+	}
+	
+	public static void deleteBooking(int ticketNumber) {
+		
+		result = false;
+		try {
+
+			Class.forName(driver);
+
+			Connection con = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+
+			PreparedStatement ps = 
+					con.prepareStatement("DELETE FROM bookedflights WHERE ticket_number=" + "'" + ticketNumber + "'");
+			
+			//ps.setInt(1, ticketNumber);
+
+			ps.executeUpdate();
+			
+			con.close();
+			
+			result = true;
+			
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+			result = false;
+
+		}
+		
+	}
+	
+	
+	
+	
 }
