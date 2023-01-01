409927475391af626266e18a24bff29ee50581fb
yovany_23 <yovany_23@yovanys-mac>
2023-01-01 12:12:19 -0500

    recoveryPassword works!

--- CIS3270Group/src/gsu/project/gui/RecoverPassword.java
+++ CIS3270Group/src/gsu/project/gui/RecoverPassword.java
@@ -18,9 +18,9 @@ public class RecoverPassword extends Application {
 	GridPane grid;
 	Scene scene;
 	Label email;
-	TextField emailInput;
+	TextField usernameInput;
 	Label confirmEmail;
-	TextField confirmEmailInput;
+	TextField confirmUsernameInput;
 	Label securityQuestion;
 	Label securityAnswer;
 	TextField securityAnswerInput;
@@ -31,105 +31,114 @@ public class RecoverPassword extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+			// TODO Auto-generated method stub
+			
+			grid = new GridPane();
+			grid.setAlignment(Pos.CENTER);
+			
+			email = new Label(" Enter your username ");
+			GridPane.setConstraints(email, 0, 0);
+			usernameInput = new TextField();
+			usernameInput.setPromptText(" Username ");
+			GridPane.setConstraints(usernameInput, 0, 1);
+			
+			confirmEmail = new Label(" Confrim your username ");
+			GridPane.setConstraints(confirmEmail, 0, 2);
+			confirmUsernameInput = new TextField();
+			confirmUsernameInput.setPromptText(" Username ");
+			GridPane.setConstraints(confirmUsernameInput, 0, 3);
+			
+			securityQuestion = new Label(" Choose Security Question");
+			GridPane.setConstraints(securityQuestion, 0, 4);
+			
+			ChoiceBox<String> choicebox = new ChoiceBox<>();
+			GridPane.setConstraints(choicebox, 0, 5);
+			
+			choicebox.getItems().add("What is your favorite color?");
 		
-		grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		
-		email = new Label(" Enter your email ");
-		GridPane.setConstraints(email, 0, 0);
-		emailInput = new TextField();
-		emailInput.setPromptText(" Email ");
-		GridPane.setConstraints(emailInput, 0, 1);
-		
-		confirmEmail = new Label(" Confrim your email ");
-		GridPane.setConstraints(confirmEmail, 0, 2);
-		confirmEmailInput = new TextField();
-		confirmEmailInput.setPromptText(" Email ");
-		GridPane.setConstraints(confirmEmailInput, 0, 3);
-		
-		securityQuestion = new Label(" Choose Security Question");
-		GridPane.setConstraints(securityQuestion, 0, 4);
-		
-		ChoiceBox<String> choicebox = new ChoiceBox<>();
-		GridPane.setConstraints(choicebox, 0, 5);
-		
-		choicebox.getItems().add("What is your favorite color?");
-	
-		securityAnswer = new Label(" Enter your Answer ");
-		GridPane.setConstraints(securityAnswer, 0, 6);
-		securityAnswerInput = new TextField();
-		securityAnswerInput.setPromptText(" Answer:");
-		GridPane.setConstraints(securityAnswerInput, 0, 7);
+			securityAnswer = new Label(" Enter your Answer ");
+			GridPane.setConstraints(securityAnswer, 0, 6);
+			securityAnswerInput = new TextField();
+			securityAnswerInput.setPromptText(" Answer:");
+			GridPane.setConstraints(securityAnswerInput, 0, 7);
 
-	    passwordRetrieve = new Button(" Retrieve Password ");
-		GridPane.setConstraints(passwordRetrieve, 0, 8);
-		
-		backButton = new Button(" <-Back ");
-		GridPane.setConstraints(backButton, 0, 9);
-		
-		displayPassword = new Label();
-		GridPane.setConstraints(displayPassword, 1, 0);
+		    passwordRetrieve = new Button(" Retrieve Password ");
+			GridPane.setConstraints(passwordRetrieve, 0, 8);
+			
+			backButton = new Button(" <-Back ");
+			GridPane.setConstraints(backButton, 0, 9);
+			
+			displayPassword = new Label();
+			GridPane.setConstraints(displayPassword, 1, 0);
 
-	    
-		grid.getChildren().addAll(email, emailInput, confirmEmail, confirmEmailInput, securityQuestion,
-				                  choicebox,securityAnswer, securityAnswerInput, passwordRetrieve, backButton);
-		
-		scene = new Scene(grid, 600,600);
-		primaryStage.setTitle("Password Recover");
-		scene.getStylesheets().add("graphicUserInterface/thing.css");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		passwordRetrieve.setOnAction( e -> {
-			String emailOne = emailInput.getText();
-			String emailTwo = confirmEmailInput.getText();
-			String answer = securityAnswerInput.getText();
+		    
+			grid.getChildren().addAll(email, usernameInput, confirmEmail, confirmUsernameInput, securityQuestion,
+					                  choicebox,securityAnswer, securityAnswerInput, passwordRetrieve, backButton);
 			
-			try {
-				checkEmailMatch (emailOne, emailTwo);
-				
-				LoginDB check = new LoginDB();
-				
-				// Change to be user name instead of email
-				String display = check.returnPassword(emailOne, answer);
-				
-				AlertBox.display("Attention", display);
-				
-				
-				
-			} catch (Exception ex) {
-				AlertBox.display("Alert", "The email address do not match!");
-			}
+			scene = new Scene(grid, 600,600);
+			primaryStage.setTitle("Password Recover");
+			scene.getStylesheets().add("graphicUserInterface/thing.css");
+			primaryStage.setScene(scene);
+			primaryStage.show();
 			
-		});
-		
-		backButton.setOnAction( e -> {
 			
-			Login login = new Login ();
-			
-			try {
-				login.start(primaryStage);
+			passwordRetrieve.setOnAction(e->{
 				
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
+				String usernameOne = usernameInput.getText();
+				String usernameTwo = confirmUsernameInput.getText();
+				String Ans = securityAnswerInput.getText();
+				
+				
+				try {
+					
+					checkUsernameMatch(usernameOne, usernameTwo);
+					
+					LoginDB check = new LoginDB();
+					
+					
+					// Change to be user name instead of email
+					String display = check.returnPassword(usernameOne,Ans);
+					
+					
+					AlertBox.display("Attention", display);
+				
+				
+				} catch(Exception ex) {
+					
+					AlertBox.display("Alert!", "The username do not match");
+				}
+				
+			});
 			
-		});
-	
-}
-	
-	public void checkEmailMatch (String email, String email2) throws Exception {
-		
-		int i = email.compareTo(email2);
-		
-		if (i > 0 || i < 0) {
-			throw new Exception ("Email Address don't match");
+			backButton.setOnAction(e->{ 
+				
+				Login login = new Login();
+				
+				try {
+					
+					login.start(primaryStage);
+					
+				} catch (Exception e1) {
+					
+					e1.printStackTrace();
+				}
+			});
+			
 		}
-		
-	}
+	
+		public void checkUsernameMatch(String un1,String un2) throws Exception {
+			
+			  int i = un1.compareTo(un2);
+			  
+			  if(i > 0 || i < 0) {
+				  throw new Exception("User name don't match");
+			  }
+			  
+			}
 
 }
